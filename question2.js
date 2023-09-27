1491b2dbc8aa7b13f341299c582f56b34d102ee6
antoniougit <bl1723r@gmail.com>
2023-09-28 00:43:38 +0300

    update to only work with the sum of 2 numbers

--- question2.js
+++ question2.js
@@ -1,12 +1,13 @@
 // Question 2: Write a javascript function that takes an array of numbers and a target number. The function should find two different numbers in the array that, when added together, give the target number. For example: answer([1,2,3], 4) should return [1,3]
 
 function findSum(numbers, targetSum) {
-    function findSubset(nums, target, index, subset) {
-        if (target === 0) {
+    function findSubset(nums, target, index, subset, count) {
+        if (target === 0 && count === 2) {
+            // returns a subset only when count is equal to 2 and the target is zero to ensure that the code finds a subset of exactly two numbers
             return subset;
         }
 
-        if (index === nums.length || target < 0) {
+        if (index === nums.length || target < 0 || count > 2) {
             return null;
         }
 
@@ -15,16 +16,23 @@ function findSum(numbers, targetSum) {
             nums,
             target - nums[index],
             index + 1,
-            [...subset, nums[index]]
+            [...subset, nums[index]],
+            count + 1
         );
 
         // exclude the current number from the subset
-        const excludeSubset = findSubset(nums, target, index + 1, subset);
+        const excludeSubset = findSubset(
+            nums,
+            target,
+            index + 1,
+            subset,
+            count
+        );
 
         return includeSubset || excludeSubset;
     }
 
-    return findSubset(numbers, targetSum, 0, []);
+    return findSubset(numbers, targetSum, 0, [], 0);
 }
 
 console.log(findSum([1, 2, 3], 4));
